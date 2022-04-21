84d0297e78eb9993fcb3e452a3b8ad0a65fccee1
Sammed Chougule <51811899+sammedchougule@users.noreply.github.com>
2022-04-21 15:54:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASeuAYr1h03X0qve5c8lUIsW3n4C9KIMo",
-  authDomain: "facebook-clone-ac461.firebaseapp.com",
-  projectId: "facebook-clone-ac461",
-  storageBucket: "facebook-clone-ac461.appspot.com",
-  messagingSenderId: "974089994818",
-  appId: "1:974089994818:web:ef99b40fb4e979c5dbe3db",
-  measurementId: "G-SSEV64VFDQ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
